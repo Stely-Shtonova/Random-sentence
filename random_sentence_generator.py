891023b0316025cfae347d60fcb5d4fa0648bd52
Stely <stelyshtonova@gmail.com>
2024-02-20 19:47:53 +0200

    Added/upgraded comments, fix overall code's logic, added an option for the computer to choose between a normal sentence and a question

--- random_sentence_generator.py
+++ random_sentence_generator.py
@@ -3,50 +3,71 @@ import random
 # creating the lists the computer will choose from
 names = ['Johny', 'Arnold', 'Emma', 'Robert', 'Daniel', 'Chris', 'Leonardo', 'Tom', 'Charles', 'Cameron',
          'Kate', 'Natalie', 'Angelina', 'Scarlet', 'Adam', 'Anne', 'Ben', 'Owen', 'Sandra', 'Jennifer', 'Taylor']
-cities = ['New York', 'Amsterdam', 'London', 'Chicago', 'Tokyo', 'Berlin', 'Paris', 'Barcelona', 'Dubai', 'Istanbul',
+places = ['New York', 'Amsterdam', 'London', 'Chicago', 'Tokyo', 'Berlin', 'Paris', 'Barcelona', 'Dubai', 'Istanbul',
           'Los Angeles', 'Rome', 'Singapore', 'Prague', 'Madrid', 'Las Vegas', 'Sydney', 'San Francisco', 'Brazil']
-verbs = ['swim in', 'walk on', 'eat', 'sleep on', 'dance with', 'write', 'listen to', 'jump off', 'dream of', 'take',
-         'install', 'learn about', 'build', 'fit', 'understand', 'pack', 'train', 'keep', 'paint', 'cook', 'slide',
-         'buy', 'share']
-nouns = ['information', 'phone', 'sock', 'bed', 'banana', 'headphones', 'cat', 'picture', 'heart', 'magazine', 'steak',
-         'agency', 'food', 'shirt', 'drama', 'relation', 'world', 'profession', 'grandmother', 'child', 'hat', 'desk']
+verbs = ['swim in', 'walk on', 'eat', 'sleep on', 'dance with', 'write to', 'listen to', 'jump off', 'dream of', 'take',
+         'install', 'learn about', 'build', 'fit in', 'understand', 'know about', 'keep', 'paint', 'prepare',
+         'talk about', 'pay for', 'share', 'sing to', 'hug', 'love', 'like', 'adore', 'hate', '', 'want', 'use', 'call',
+         'think about', 'clean']
+nouns = ['information', 'phone', 'sock', 'pizza', 'banana', 'headphones', 'cat', 'picture', 'heart', 'magazine',
+         'steak', 'agency', 'food', 'shirt', 'drama', 'student', 'world', 'professor', 'grandmother', 'child', 'hat',
+         'desk', 'horse', 'tomatoes', 'finger', 'family', 'history', 'system', 'music', 'movie', 'woman', 'payment',
+         'pie', 'tea', 'coffee']
 adjectives = ['civil', 'beautiful', 'large', 'red', 'colorful', 'united', 'soft', 'golden', 'delicious', 'bald',
-              'plain',
-              'chubby', 'clean', 'fancy', 'attractive', 'hot', 'skinny', 'kind', 'grumpy', 'polite', 'fluffy']
+              'cute', 'chubby', 'clean', 'fancy', 'attractive', 'hot', 'skinny', 'kind', 'grumpy', 'polite', 'long',
+              'angry', 'rude']
 adverbs = ['slowly', 'coldly', 'sadly', 'sincerely', 'loudly', 'quickly', 'calmly', 'truly', 'gently', 'perfectly',
-           'happily',
-           'softly', 'carefully', 'bravely', 'angrily', 'easily', 'cheerfully', 'eagerly']
-places = ['downstairs', 'outside', 'on the beach', 'in the bookstore', 'on the bed', 'in the shower', 'on the sofa',
-          'in school', 'in the park', 'in the cinema', 'around the block', 'in the sea', 'in the hotel room',
-          'over there']
+           'happily', 'softly', 'carefully', 'bravely', 'angrily', 'easily', 'cheerfully', 'eagerly', 'surprisingly']
 
 
 def random_sentence():
     print('Your first sentence/s is/are:\n--------------')
     while True:
-        # letting the computer choose the count of iterations and the count of the subjects
-        sentences_count = random.choice(range(1, 5))
-        subject_count = random.randint(1, 2)
+        # letting the computer choose the count of iterations/sentences
+        sentences_count = random.randint(1, 3)
         for current_sentence in range(sentences_count):
-            name1 = random.choice(names)
-            name2 = random.choice(names)
-            city = random.choice(cities)
+            # chooses number of subjects and the type of the sentence
+            type_of_sentence = random.choice(['question', 'simple'])
+            subject_count = random.randint(1, 2)
+            if subject_count == 1:
+                name1 = random.choice(names)
+            else:
+                name1 = random.choice(names)
+                name2 = random.choice(names)
+            # the computer chooses a random word from each of the sequences
             verb = random.choice(verbs)
             noun = random.choice(nouns)
             adjective = random.choice(adjectives)
             adverb = random.choice(adverbs)
             place = random.choice(places)
-            # checking if the subject is only one -> checks/fixes grammar
-            if subject_count == 1:
-                verb_list = verb.split()
-                if len(verb_list) == 2:
-                    verb_, preposition = verb_list[0], verb_list[1]
-                    print(f"{name1} from {city} {adverb} {verb_+'s '+ preposition} {'the ' +adjective} {noun} {place}!")
+            # checks if the sentence is a question and prints it
+            if type_of_sentence == 'question':
+                question_word = random.choice(['Who', 'Why', 'When', 'How'])
+                details = random.choice(['often', 'many times', 'long'])
+                # checking if the subject is only one -> checks/fixes grammar
+                if subject_count == 1:
+                    if question_word != 'How':
+                        print(f"{question_word} does {name1} {verb} the {noun}?")
+                    else:
+                        print(f"{question_word} {details} does {name1} {verb} the {noun}?")
                 else:
-                    print(f"{name1} from {city} {adverb} {verb}s {'the ' +adjective} {noun} {place}!")
+                    if question_word != 'How':
+                        print(f"{question_word} do {name1} and {name2} {verb} the {noun}?")
+                    else:
+                        print(f"{question_word} {details} do {name1} and {name2} {verb} the {noun}?")
             else:
-                print(f"{name1} and {name2} from {city} {adverb} {verb} {'the ' +adjective} {noun} {place}!")
-        # asking the user for directions
+                # checking if the subject is only one -> checks/fixes grammar
+                if subject_count == 1:
+                    verb_list = verb.split()
+                    if len(verb_list) == 2:
+                        verb_, preposition = verb_list[0], verb_list[1]
+                        print(
+                            f"{name1} {adverb} {verb_ + 's ' + preposition} {'the ' + adjective} {noun} in {place}.")
+                    else:
+                        print(f"{name1} {adverb} {verb}s {'the ' + adjective} {noun} in {place}.")
+                else:
+                    print(f"{name1} and {name2} {adverb} {verb} {'the ' + adjective} {noun} in {place}.")
+        # prompting the user for directions
         answer = input("Click [Enter] for more or [S] to stop the program: ")
         if answer.lower() == 's':
             break
